d313ea69a310018d7dc7a9dbd76f8e6590067cba
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-12 23:46:45 +0200

    implement header elements

---
+++ src/#2 HTML Design Patterns/2-04. Header Elements/HeaderElements.jsx
@@ -0,0 +1,7 @@
+export function HeaderElements() {
+  return (
+    <div className="header-elements">
+      <h1>Header Elements</h1> <p>Look at the README to see the header</p>
+    </div>
+  )
+}
---
+++ src/#2 HTML Design Patterns/2-04. Header Elements/__tests__/HeaderElements.int.test.js
@@ -0,0 +1,9 @@
+import { regular } from './HeaderElements.int.story'
+
+describe('<HeaderElements />', () => {
+  describe('Render', () => {
+    test('must match regular()', () => {
+      expect(global.renderToJSON(regular())).toMatchSnapshot()
+    })
+  })
+})

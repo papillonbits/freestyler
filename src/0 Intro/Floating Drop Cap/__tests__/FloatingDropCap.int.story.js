e16a81c1633770fe3241c1053c0c0764a644c913
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2023-07-02 17:09:00 +0200

    rename 0 intro

--- src/0 Intro/Floating Drop Cap/__tests__/FloatingDropCap.int.story.js
+++ src/0 Intro/Floating Drop Cap/__tests__/FloatingDropCap.int.story.js
@@ -3,7 +3,7 @@ import { FloatingDropCap } from '../FloatingDropCap'
 import results from '../../../../.jest-test-results.json'
 
 export default {
-  title: '#0 Intro/Floating Drop Cap',
+  title: '0 Intro/Floating Drop Cap',
   component: FloatingDropCap,
   decorators: [withTests({ results })],
   parameters: { jest: ['FloatingDropCap.int.test.js'] },
